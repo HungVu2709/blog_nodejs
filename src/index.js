d10f4bfee1875cb64e7657e64c81a07de9f58f70
Hung <vuhuyhung2792000@gmail.com>
2023-09-19 17:23:18 +0700

    Refactor code:create routes, app/controllers

--- src/index.js
+++ src/index.js
@@ -3,31 +3,31 @@ const morgan = require("morgan");
 const handlebars = require("express-handlebars");
 const path = require("path");
 
+const route = require("./routes");
+
 const app = express();
 const port = 3000;
+
+// truy cap rq.body
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+app.use(express.json());
 
 // app.use(express.static(path.join(__dirname, "public")));
 app.use(express.static(__dirname + "/public"));
 
 app.use(morgan("combined"));
 
-//Template Engine
+//Template Engine hbs
 app.engine("hbs", handlebars.engine({ extname: ".hbs" }));
 app.set("view engine", "hbs");
 app.set("views", path.join(__dirname, "resources/views"));
 
-//Routes
-app.get("/", (req, res) => {
-  res.render("home");
-});
-
-app.get("/news", (req, res) => {
-  res.render("news");
-});
-
-app.get("/about", (req, res) => {
-  res.render("about");
-});
+//Routes init
+route(app);
 
 // 127.0.0.1 - localhost:3000
 app.listen(port, () => {
